4a7b338bba172453b39d6cd9ed70834a2dd53d6e
Taitoy <c0a22150fa@edu.teu.ac.jp>
2023-04-18 17:26:32 +0900

    演習１まで

--- pygame_image.py
+++ pygame_image.py
@@ -7,7 +7,11 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kk_img = pg.transform.flip(pg.image.load("ex01/fig/3.png"), True, False)
-    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
+    kk_imgs = [kk_img, kk_img, kk_img, kk_img, kk_img, kk_img, kk_img, kk_img, kk_img, kk_img,
+    pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), 
+    pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), 
+    pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0),
+    pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0)]
     tmr = 0
 
     while True:
@@ -16,9 +20,11 @@ def main():
      
         tmr += 1
         x = tmr%1600
+        
+
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img, [1600-x, 0])
-        screen.blit(kk_imgs[x%2], [300, 200]) 
+        screen.blit(kk_imgs[x%26], [300, 200]) 
 
         pg.display.update()
         clock.tick(100)
