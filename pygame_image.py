37f6f3cff642fb01e9a646dd9c15728c662b2f37
Taitoy <c0a22150fa@edu.teu.ac.jp>
2023-04-18 17:46:13 +0900

    完成

--- pygame_image.py
+++ pygame_image.py
@@ -6,6 +6,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_imgs=pg.transform.flip(bg_img, True, False)
     kk_img = pg.transform.flip(pg.image.load("ex01/fig/3.png"), True, False)
     kk_imgs = [kk_img, kk_img, kk_img, kk_img, kk_img, kk_img, kk_img, kk_img, kk_img, kk_img,
     pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), pg.transform.rotozoom(kk_img, 10, 1.0), 
@@ -19,11 +20,12 @@ def main():
             if event.type == pg.QUIT: return
      
         tmr += 1
-        x = tmr%1600
+        x = tmr%3200
         
 
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x, 0])
+        screen.blit(bg_imgs, [1600-x, 0])
+        screen.blit(bg_img, [3200-x, 0])
         screen.blit(kk_imgs[x%26], [300, 200]) 
 
         pg.display.update()
